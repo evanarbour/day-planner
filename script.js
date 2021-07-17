a99fa2d7d43e4d4b7c345792d7c9bd00f1c74a17
Evan Arbour <evan.arbour@gmail.com>
2021-07-17 15:34:56 -0700

    created html elements via javascript

--- script.js
+++ script.js
@@ -1,16 +1,87 @@
+var plannerRow = $('.container')
+var hour = '';
+// this creates the header with the current day
 var today = moment();
 $('#currentDay').text(today.format('dddd, MMMM Do, YYYY'));
 
+// creates a variable of the current time in HOUR
 var currentHour = moment().format('H');
-console.log(currentHour);
 
-function displaycurrentHour(currentHour) {
-    // if the hour is equal to the current hour, set the class to "present"
+// specifies the hours of the planner: 9:00am-5:00pm (17:00)
+function displayRows() {
+    for (let hour = 9; hour <= 17; hour++) {
+        var displayHour = 0;
+        var ampm = '';
+        if (hour > 12) {
+            displayHour = hour - 12;
+            ampm = 'pm';
+        } else if (hour == 12) {
+            displayHour = hour;
+            ampm = 'pm';
+        } else {
+            displayHour = hour;
+            ampm = 'am';
+        };
+    
+    //create rows for the planner with class of "row" to connect to Bootstrap styling.
+    let $rowDiv = $('<div>');
+    $rowDiv.addClass('row');
+
+    // create columns for the time element of planner
+    let $columnTimeDiv = $('<div>');
+    $columnTimeDiv.addClass('col-1 hour');
+
+    // create the a <span> element to display the number value of the hour
+    let $timeDisplay = $('<span>')
+    $timeDisplay.text(`${displayHour} ${ampm}`);
+
+    // create columns for the event element of the planner
+    let $columnEventWrapper = $('<div>');
+    $columnEventWrapper.addClass('col-10 input-column');
+
+    // create input field
+    let $eventInputField = $('<input type="text">')
+
+    let $saveBtn = $('<div>');
+    $saveBtn.addClass('saveBtn col-1');
+    $saveBtn.text('Save Event');
+    
+    // append time elements to HTML
+    plannerRow.append($rowDiv);
+    $rowDiv.append($columnTimeDiv);
+    $columnTimeDiv.append($timeDisplay);
+    
+    // append event elements to HTML
+    plannerRow.append($rowDiv);
+    $rowDiv.append($columnEventWrapper);
+    $columnEventWrapper.append($eventInputField);
+    
+    // append save button to HTML
+    plannerRow.append($rowDiv);
+    $rowDiv.append($saveBtn);
+
+    };
+};
+
+displayRows();
+
+
+// this function changes the color of the rows depending on the time
+function changeRowColor($columnEventWrapper, hour) {
+    if (currentHour > hour) {
+        $columnEventWrapper.addClass('past');
+    }
+};
+
+changeRowColor();
+
+
+// if the hour is equal to the current hour, set the class to "present"
 
     // if the hour is less than the current hour, set it to "past"
 
     // if the hour is more than the current hour, set it to "future"
-}
+
 // use moment.js to connect time of day to class of "past", "present" and "future" to create stylings on webpage
 // moment().toObject() ?? returns an object containing time and can set that equal to variable and then if statement?
 
