0dd533900064be8bf579e7890f5f66ba7ae34fbe
Evan Arbour <evan.arbour@gmail.com>
2021-07-19 13:18:35 -0700

    added comments to js file

--- script.js
+++ script.js
@@ -19,18 +19,23 @@ function createRows() {
         plannerContainer.append(rowDiv);
         
     }
-        // creates save button click event and sets input to localStorage
+        // find the save button, and create a click event that sets input to localStorage
         $("button.btn.btn-success").click(function(){ 
        
+        // create a variable that connects to the specific clicked button on the page and takes its id
         var buttonID = $(this).data('id');
+        
+        // navigated through the HTML to find the input value typed by the user.
+        // .parent = columnSaveBtn, .siblings = other columns in the rowDiv, search other columns for "input", select the value (user input)
         var inputText = $(this).parent().siblings().find("input").val();
 
+        // set the data to a key:value pair and save in localStorage
         localStorage.setItem(buttonID, inputText);
         event.preventDefault();
     });
 };
 
-
+// creates the "am" or "pm" after the displayed hour of the planner
 function displayAmPm (hour) {
     var ampm = '';
     var displayHour = 0;
@@ -47,9 +52,11 @@ function displayAmPm (hour) {
     return displayHour + '' + ampm
 };
 
+// changes the color of the row based on the hour
 function changeRowColor() {
-    // creates a variable of the current time in HOUR
+    // creates a variable of the current time in HOURS
     var currentHour = parseInt(moment().format('H'));
+    // make a for loop to sort through the hours on the planner
     for (var i = 9; i <= 17; i++) {
         if (currentHour > i) {
          $(`#inputText${i}`).addClass('past');
@@ -62,9 +69,11 @@ function changeRowColor() {
 
 }
 
-
+// display any previously stored data on the page 
 function getLocalStorage() {
+    // sort through the hours in the planner
     for (var i = 9; i <= 17; i++) {
+        // display the stored data to the appropriate times
         $(`#inputText${i}`).val(localStorage.getItem(i));
     }
 };
