cd894ba02efc2211f287d368b7af89d3dba5ce1b
Evan Arbour <evan.arbour@gmail.com>
2021-07-19 13:11:20 -0700

    updated js file and styling colors in CSS

--- script.js
+++ script.js
@@ -1,83 +1,77 @@
-var plannerRow = $('.container')
-var hour = '';
+var plannerContainer = $('.container');
+var hour;
 // this creates the header with the current day
 var today = moment();
 $('#currentDay').text(today.format('dddd, MMMM Do, YYYY'));
 
-// creates a variable of the current time in HOUR
-var currentHour = parseInt(moment().format('H'));
 
-
-
-// specifies the hours of the planner: 9:00am-5:00pm (17:00)
-function displayRows() {
-    for (let hour = 9; hour <= 17; hour++) {
-        var displayHour = 0;
-        var ampm = '';
-        if (hour > 12) {
-            displayHour = hour - 12;
-            ampm = 'pm';
-        } else if (hour == 12) {
-            displayHour = hour;
-            ampm = 'pm';
-        } else {
-            displayHour = hour;
-            ampm = 'am';
-        };
-    
-    //create rows for the planner with class of "row" to connect to Bootstrap styling.
-    let $rowDiv = $('<div>');
-    $rowDiv.addClass('row');
-
-    // create columns for the time element of planner
-    let $columnTimeDiv = $('<div>');
-    $columnTimeDiv.addClass('col-1 hour');
-
-    // create a <span> element to display the number value of the hour
-    let $timeDisplay = $('<span>')
-    $timeDisplay.text(`${displayHour} ${ampm}`);
-
-    // create columns for the event element of the planner
-    let $columnEventWrapper = $('<div>');
-    $columnEventWrapper.addClass('col-10 input-column');
-
-    // create input field
-    let $eventInputField = $('<input type="text">')
-
-    // create save button
-    let $saveBtn = $('<div>');
-    $saveBtn.addClass('saveBtn col-1');
-    $saveBtn.text('Save Event');
-    
-    // append time elements to HTML
-    plannerRow.append($rowDiv);
-    $rowDiv.append($columnTimeDiv);
-    $columnTimeDiv.append($timeDisplay);
-    
-    // append event elements to HTML
-    plannerRow.append($rowDiv);
-    $rowDiv.append($columnEventWrapper);
-    $columnEventWrapper.append($eventInputField);
-    
-    // append save button to HTML
-    plannerRow.append($rowDiv);
-    $rowDiv.append($saveBtn);
-
-    // color each row based on current time
-    $columnEventWrapper.each(function() {
-        if (currentHour > hour ) {
-            $columnEventWrapper.addClass('past');
-        } else if (currentHour < hour) {
-            $columnEventWrapper.addClass('future');
-        } else {
-            $columnEventWrapper.addClass('present');
-        }
+// dynamically creates planner in browser 
+function createRows() {
+    // sets the hour to a variable and loops through from 9-17 (5pm)
+    for (let i = 9; i <= 17; i++) {
+        rowDiv = $(`<div class="row">`);
+        columnTime = $(`<div class="col-1 hour">${displayAmPm(i)}</div>`);
+        columnInput = $(`<div class="col-10"><input data-input="${i}" id="inputText${i}" class="inputText input-field" type="text" name="userInput"></div>`);
+        columnSaveBtn = $(`<div class="col-1"><button data-id="${i}" id="savePlanner" class="btn btn-success"><i class="fas fa-save"></i>Save</button></div>`);
+        rowDiv.append(columnTime);
+        rowDiv.append(columnInput);
+        rowDiv.append(columnSaveBtn);
+        plannerContainer.append(rowDiv);
         
+    }
+        // creates save button click event and sets input to localStorage
+        $("button.btn.btn-success").click(function(){ 
+       
+        var buttonID = $(this).data('id');
+        var inputText = $(this).parent().siblings().find("input").val();
+
+        localStorage.setItem(buttonID, inputText);
+        event.preventDefault();
     });
-    
+};
+
+
+function displayAmPm (hour) {
+    var ampm = '';
+    var displayHour = 0;
+    if (hour > 12) {
+    displayHour = hour - 12;
+    ampm = 'pm';
+    } else if (hour == 12) {
+    displayHour = hour
+    ampm = 'pm';
+    } else {
+    displayHour = hour
+    ampm = 'am';
+    }
+    return displayHour + '' + ampm
+};
+
+function changeRowColor() {
+    // creates a variable of the current time in HOUR
+    var currentHour = parseInt(moment().format('H'));
+    for (var i = 9; i <= 17; i++) {
+        if (currentHour > i) {
+         $(`#inputText${i}`).addClass('past');
+        } else if (currentHour < i) {
+            $(`#inputText${i}`).addClass('future');
+        } else {
+            $(`#inputText${i}`).addClass('present');
+        }
+    }
+
+}
+
+
+function getLocalStorage() {
+    for (var i = 9; i <= 17; i++) {
+        $(`#inputText${i}`).val(localStorage.getItem(i));
     }
 };
 
-displayRows();
+createRows();
+changeRowColor();
+getLocalStorage();
+
 
 
